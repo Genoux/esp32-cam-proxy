537ea11583a76eda3220a4f8f77e934992c81218
John Barry <john.olivierb@gmail.com>
2023-11-12 23:10:04 -0500

    Add log

--- server.ts
+++ server.ts
@@ -1,24 +1,41 @@
 import express from 'express';
-import 'dotenv/config';
-import { Server as SocketIOServer } from 'socket.io';
-import http from 'http';
-import axios from 'axios';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 const app = express();
-const server = http.createServer(app);
-const io = new SocketIOServer(server);
 const PORT = process.env.PORT || 3000;
 
+// Proxy middleware configuration
 const espCameraProxy = createProxyMiddleware({
   target: process.env.ESP32_CAM_URL,
-  changeOrigin: true
+  changeOrigin: true,
+  logLevel: 'debug', // Enable logging for the proxy
+  onProxyReq: (proxyReq, req, res) => {
+    console.log(`[Proxy] Request to: ${req.url}`);
+  },
+  onProxyRes: (proxyRes, req, res) => {
+    console.log(`[Proxy] Received response for: ${req.url}`);
+  },
+  onError: (err, req, res) => {
+    console.error(`[Proxy] Error: ${err.message}`);
+    res.writeHead(500, {
+      'Content-Type': 'text/plain',
+    });
+    res.end('Something went wrong with the proxy.');
+  }
 });
 
+// Enable CORS for all routes
 app.use(cors());
-app.use(espCameraProxy);
 
-server.listen(PORT, () => {
+// Apply the proxy middleware
+app.use('/', espCameraProxy);
+
+// Start the server
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
